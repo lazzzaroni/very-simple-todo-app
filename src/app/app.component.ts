100fdbc6e2a80f682a8d20dab1ff31e2a8b8e8be
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-05-13 18:16:56 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'simple-todo-app';
+export class AppComponent implements OnInit {
+  // State
+  header = 'Very Simple Todo App';
+  todos: string[] = [];
+  todoText!: string;
+
+  // Events
+  addTodo() {
+    this.todos.push(this.todoText);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.todoText = '';
+  }
+
+  // Lifecycle
+  ngOnInit() {
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos as string) || [];
+  }
 }
